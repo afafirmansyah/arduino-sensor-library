28228c85cb87f031676e0d3cc3dbb4d6a9469bf8
Ahmad Fauzi Firmansyah <60831444+afafirmansyah@users.noreply.github.com>
2023-04-11 13:50:47 +0700

    Add files via upload

---
+++ KY040_MQTT.ino
@@ -0,0 +1,128 @@
+#include "Encoder.h"
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
+#include <AsyncMqttClient.h>
+
+#define WIFI_SSID "Ahmad Fauzi Firmansyah"
+#define WIFI_PASSWORD "Pimen2122"
+
+// Raspberri Pi Mosquitto MQTT Broker
+//#define MQTT_HOST IPAddress(192, 168, 1, XXX)
+// For a cloud MQTT broker, type the domain name
+#define MQTT_HOST "broker.hivemq.com"
+#define MQTT_PORT 1883
+
+// Temperature MQTT Topics
+#define MQTT_PUB_ENCODER "afafirmansyah/esp/dht/encoder"
+
+Encoder myEnc(4, 5);  // Avoid using pins with LEDs attached
+long oldPosition  = -999;
+#define BTN_3 0
+
+AsyncMqttClient mqttClient;
+Ticker mqttReconnectTimer;
+
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
+Ticker wifiReconnectTimer;
+
+unsigned long previousMillis = 0;   // Stores last time temperature was published
+const long interval = 10000;        // Interval at which to publish sensor readings
+
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void onWifiConnect(const WiFiEventStationModeGotIP& event) {
+  Serial.println("Connected to Wi-Fi.");
+  connectToMqtt();
+}
+
+void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
+  Serial.println("Disconnected from Wi-Fi.");
+  mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+  wifiReconnectTimer.once(2, connectToWifi);
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    mqttReconnectTimer.once(2, connectToMqtt);
+  }
+}
+
+/*void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+  }
+
+  void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  }*/
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.print("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
+  pinMode(BTN_3, INPUT);
+
+  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  //mqttClient.onSubscribe(onMqttSubscribe);
+  //mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  // If your broker requires authentication (username and password), set them below
+  //mqttClient.setCredentials("REPlACE_WITH_YOUR_USER", "REPLACE_WITH_YOUR_PASSWORD");
+
+  connectToWifi();
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  // Every X number of seconds (interval = 10 seconds)
+  // it publishes a new MQTT message
+  if (currentMillis - previousMillis >= interval) {
+    // Save the last time a new reading was published
+    previousMillis = currentMillis;
+    // New DHT sensor readings
+    long newPosition = myEnc.read();
+    if (newPosition != oldPosition) {
+      oldPosition = newPosition;
+      Serial.print("New Position: ");
+      Serial.println(newPosition);
+    }
+
+    // Publish an MQTT message on topic esp/dht/temperature
+    uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_ENCODER, 1, true, String(newPosition).c_str());
+    Serial.printf("Publishing on topic %s at QoS 1, packetId: %i ", MQTT_PUB_ENCODER, packetIdPub1);
+    Serial.printf("Message: %.2f \n", newPosition);
+  }
+}
